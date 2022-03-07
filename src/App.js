58c077d75c6e16075193d0613d2b3433502ce1fc
chris-base <baseappsdev@gmail.com>
2022-03-06 20:38:56 -0600

    started todolist controller layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import NewTodoComponent from "./Components/NewTodoComponent";
 import TitleDarkLightComponent from "./Components/TitleDarkLightComponent";
 import TodoListComponent from "./Components/TodoListComponent";
+import TodoListControllerComponent from "./Components/TodoListControllerComponent";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <NewTodoComponent />
 
         <TodoListComponent />
+
+        <TodoListControllerComponent />
       </div>
     </div>
   );
---
+++ src/Components/TodoListControllerComponent.js
@@ -0,0 +1,25 @@
+import "../Styles/TodoListControllerStyles.css";
+
+const TodoListControllerComponent = () => {
+  return (
+    <div id='listControllerComponent'>
+      <div>
+        <p className='controllerText'>items left</p>
+      </div>
+
+      <div id='controllerViewableContainer'>
+        <p className='controllerViewable'>All</p>
+        <p className='controllerViewable'>Active</p>
+        <p className='controllerViewable'>Completed</p>
+      </div>
+
+      <div>
+        <p className='controllerText' style={{ cursor: "pointer" }}>
+          Clear Completed
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TodoListControllerComponent;
